adc51904050759ddd83c4cb740025b1d31b5bdfc
BERKANE <chihab.berkane@cgi.com>
2020-07-06 14:58:19 +0200

    maintenant nous avons un controller

---
+++ libOnline/src/Controller/HomeController.php
@@ -0,0 +1,24 @@
+<?php
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+
+class HomeController extends AbstractController
+{
+
+    /**
+    * @Route("/home")
+    */
+    public function home()
+    {
+        $number = random_int(0, 100);
+
+        return $this->render('home/home.html.twig', [
+            'number' => $number,
+        ]);
+
+    }
+}
